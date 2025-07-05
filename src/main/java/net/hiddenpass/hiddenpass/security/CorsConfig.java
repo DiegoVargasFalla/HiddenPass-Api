f8ab0fee6f48868e5935455ec9eb23b518dde010
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-07-05 14:46:40 -0300

    update cors

--- src/main/java/net/hiddenpass/hiddenpass/security/CorsConfig.java
+++ src/main/java/net/hiddenpass/hiddenpass/security/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173/")); // http://localhost:5173
+        config.setAllowedOrigins(List.of("/**")); // http://localhost:5173
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
